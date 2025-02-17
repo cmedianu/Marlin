438f684aa22340cba76c674be1507e597d2fc99f
Calin Medianu <488889+cmedianu@users.noreply.github.com>
2025-02-17 09:36:34 +0000

    WIP

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -134,7 +134,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 Neo"
+#define CUSTOM_MACHINE_NAME "Our Ender-3 Neo"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
